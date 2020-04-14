cb3350cd2e7fcaea3e6b1aa9ed0a684c7e59ea68
Ariane Evans <ariane.evans@student.makeschool.com>
2020-04-14 15:53:33 -0700

    [add] photos

--- src/badge.js
+++ src/badge.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function Badge(props) {
-    const {name, description, category} = props.badge
+    const {name, description, category, image} = props.badge
       return (
         <div className="Badge">
+            <img src={`${process.env.PUBLIC_URL}${image}`} width="200" height="200" />
             <h3>{name}</h3>
             <h4>{category}</h4>
             <p>{description}</p>
